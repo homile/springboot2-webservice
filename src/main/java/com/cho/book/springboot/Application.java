2ed94c8d53cd383ed9c03246885c9cba44d10105
Homile <ssanho2@naver.com>
2021-02-01 17:49:44 +0900

    4.2 머스테치 사용 (21.02.01)

--- src/main/java/com/cho/book/springboot/Application.java
+++ src/main/java/com/cho/book/springboot/Application.java
@@ -2,8 +2,10 @@ package com.cho.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /* main class(Application.class) */
+@EnableJpaAuditing  // JPA Auditing 활성화
 @SpringBootApplication  // 스프링부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
 public class Application {
     public static void main(String[] args){
