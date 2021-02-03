f54900b26e0b5173df0269f59bd6e5e937db1b61
Homile <ssanho2@naver.com>
2021-02-03 15:26:22 +0900

    4.2 머스테치 사용 (21.02.03)

--- src/main/java/com/cho/book/springboot/web/PostsApiController.java
+++ src/main/java/com/cho/book/springboot/web/PostsApiController.java
@@ -13,7 +13,7 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
@@ -24,7 +24,7 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("api/v1/posts/{id}")
+    @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id){
         return postsService.findById(id);
     }
